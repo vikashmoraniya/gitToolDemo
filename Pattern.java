f21d9ff26c938225844f5dc2c325cc58adc3ef3d
vikashmoraniya <vikashmoraniya6266@gmail.com>
2022-12-10 15:16:49 +0530

    new file added

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+class pattern
+{
+	public static void main(String[] args) 
+	{
+		int i,j,number=5;
+	    for (i=0;i<=number;i++) 
+	  {
+	    	for (j=i;j<=number;i++) 
+	        {
+			    System.out.print("*");
+			    System.out.println(" ");					
+		    } 		
+	  }		
+	}
+	
+}
